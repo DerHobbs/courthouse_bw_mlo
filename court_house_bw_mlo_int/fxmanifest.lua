06ca916f361be5cc2f7b372c92feb7baca487afd
Hobbs <101003021+DerHobbs@users.noreply.github.com>
2022-06-30 04:38:03 +0200

    Add files via upload

---
+++ court_house_bw_mlo_int/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "adamant"
+game "rdr3"
+author 'Hobbs@RisingSunRP'
+description 'Blackwater Court House Interior'
+
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+
+dependency 'objectloader'
+
+files {
+	'bw_mayor_int.xml'
+}
+
+objectloader_maps {
+	'bw_mayor_int.xml'
+}
